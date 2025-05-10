d280a5ad1a3d048b4471ddc0a5e2487e34c38ee6
Todor Balabanov <todor.balabanov@gmail.com>
2025-05-10 14:11:48 +0300

    Tree trace has been implemented.

--- Main.java
+++ Main.java
@@ -5,10 +5,20 @@ import java.util.Optional;
 
 public class Main {
 	private static enum Tile {
-		E,
-		O,
-		X,
-	};
+		E( 0),
+		O(-1),
+		X(+1);
+
+		private int value;
+
+		private Tile(int value) {
+			this.value = value;
+		}
+
+		public int value() {
+			return value;
+		}
+	}
 
 	private static final int NONE = 0;
 	private static final int MIN = -1;
@@ -131,17 +141,60 @@ public class Main {
 		return Optional.empty();
 	}
 
+	private static void print(int board[][]) {
+		String text = Arrays.deepToString( board );
+		text = text.replace("],", "],\n");
+		text = text.replace(""+MIN, "O");
+		text = text.replace(""+MAX, "X");
+		text = text.replace(""+NONE, "-");
+		text = text.replace(",", "");
+		text = text.replace("[", " ");
+		text = text.replace("]", " ");
+		text = text.replace(" ", "");
+		//text = text.replace("-", " ");
+		System.out.println( text );
+	}
+
+	private static int trace(int board[][], int player, int depth) {
+		Optional<Integer> score = hasWinner( board );
+		if(score.isEmpty() == false) {
+			return score.get();
+		}
+
+		int sum = 0;
+		for(int i=0; i<board.length; i++) {
+			for(int j=0; j<board[i].length; j++) {
+				if(board[i][j] != NONE) {
+					continue;
+				}
+
+				board[i][j] = player;
+				int next = NONE;
+				if(player == MAX) {
+					next = MIN;
+				}
+				if(player == MIN) {
+					next = MAX;
+				}
+				sum += trace(board, next, depth+1);
+				board[i][j] = NONE;
+			}
+		}
+
+		/**/
+		if(depth == 1) {
+			print( board );
+			System.out.println(sum);
+			System.out.println();
+		}
+		/**/
+
+		return sum;
+	}
+
 	public static void main(String args[]) {
 		reset( board );
 
-		board = new int[][] {
-			{-1, +1, -1,},
-			{+1, +1, -1,},
-			{-1, -1, +1,},
-		};
-
-		System.out.println(
-		    Arrays.deepToString(board).replace("],","],\n") );
-		System.out.println( hasWinner(board) );
+		trace(board, MAX, 0);
 	}
 }
